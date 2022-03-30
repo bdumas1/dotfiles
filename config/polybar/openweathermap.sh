673c87c375baa01ee73472494e870e517f6fcef1
Benoit DUMAS <benoit@sogilis.com>
2022-03-30 08:52:37 +0200

    polybar: add openweathermap script

---
+++ config/polybar/openweathermap.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+get_icon() {
+    case $1 in
+        # Icons for weather-icons
+        01d) icon="";;
+        01n) icon="";;
+        02d) icon="";;
+        02n) icon="";;
+        03*) icon="";;
+        04*) icon="";;
+        09d) icon="";;
+        09n) icon="";;
+        10d) icon="";;
+        10n) icon="";;
+        11d) icon="";;
+        11n) icon="";;
+        13d) icon="";;
+        13n) icon="";;
+        50d) icon="";;
+        50n) icon="";;
+        *) icon="";
+
+        # Icons for Font Awesome 5 Pro
+        #01d) icon="";;
+        #01n) icon="";;
+        #02d) icon="";;
+        #02n) icon="";;
+        #03d) icon="";;
+        #03n) icon="";;
+        #04*) icon="";;
+        #09*) icon="";;
+        #10d) icon="";;
+        #10n) icon="";;
+        #11*) icon="";;
+        #13*) icon="";;
+        #50*) icon="";;
+        #*) icon="";
+    esac
+
+    echo $icon
+}
+
+get_duration() {
+
+    osname=$(uname -s)
+
+    case $osname in
+        *BSD) date -r "$1" -u +%H:%M;;
+        *) date --date="@$1" -u +%H:%M;;
+    esac
+
+}
+
+KEY="f131d5449d5a1995d985d2d77b3a28d6"
+CITY="2968985"
+UNITS="metric"
+SYMBOL="°C"
+
+API="https://api.openweathermap.org/data/2.5"
+
+if [ -n "$CITY" ]; then
+    if [ "$CITY" -eq "$CITY" ] 2>/dev/null; then
+        CITY_PARAM="id=$CITY"
+    else
+        CITY_PARAM="q=$CITY"
+    fi
+
+    current=$(curl -sf "$API/weather?appid=$KEY&$CITY_PARAM&units=$UNITS")
+else
+    location=$(curl -sf https://location.services.mozilla.com/v1/geolocate?key=geoclue)
+
+    if [ -n "$location" ]; then
+        location_lat="$(echo "$location" | jq '.location.lat')"
+        location_lon="$(echo "$location" | jq '.location.lng')"
+
+        current=$(curl -sf "$API/weather?appid=$KEY&lat=$location_lat&lon=$location_lon&units=$UNITS")
+    fi
+fi
+
+if [ -n "$current" ]; then
+    current_temp=$(echo "$current" | jq ".main.temp" | cut -d "." -f 1)
+    current_icon=$(echo "$current" | jq -r ".weather[0].icon")
+
+    echo "$(get_icon "$current_icon") $current_temp$SYMBOL"
+fi
