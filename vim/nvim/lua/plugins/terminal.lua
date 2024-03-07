17ac786f0b19379013f4633783aa3f295e66a087
Benoit DUMAS <benoit@sogilis.com>
2024-03-07 10:50:07 +0100

    nvim: add terminal toggle

---
+++ vim/nvim/lua/plugins/terminal.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        opts = {
+                open_mapping = [[<leader>t]],
+                insert_mappings = false,
+                shell = "zsh",
+        },
+    },
+}
