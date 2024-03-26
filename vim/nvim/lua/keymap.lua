b676498783df8426671ca6e989fbd3055a461376
Benoit DUMAS <benoit@sogilis.com>
2024-03-26 08:56:46 +0100

    nvim: update config

--- vim/nvim/lua/keymap.lua
+++ vim/nvim/lua/keymap.lua
@@ -15,23 +15,26 @@ local telescopeOpts = {
 }
 
 local findFile = function ()
-    -- builtin.find_files(require('telescope.themes').get_dropdown(telescopeOpts))
     builtin.find_files(telescopeOpts)
 end
+
 local searchFile = function ()
-    -- builtin.live_grep(require('telescope.themes').get_dropdown(telescopeOpts))
     builtin.live_grep(telescopeOpts)
+end
+
+local insertAndIndent = function()
+	return string.match(vim.api.nvim_get_current_line(), "%g") == nil and "cc" or "i"
 end
 
 -- File navigation
 vim.keymap.set('n', '<C-p>', findFile, {desc = 'Find in Files'})
-vim.keymap.set('n', '<C-S-F>', searchFile, {desc = 'Files'})
+vim.keymap.set('n', '<C-S-F>', searchFile, {desc = 'Files'})
 vim.keymap.set('n', '<leader>fr', builtin.buffers, {desc = 'Find buffer'})
-vim.keymap.set('n', '<C-b>', ":Neotree toggle=true reveal<CR>", {desc = 'File explorer'})
-vim.keymap.set('n', '<leader>b', ":Neotree reveal<CR>", {desc = 'File explorer reveal'})
 
 -- Editor action
 vim.keymap.set('n', '<leader>r', ':IncRename ', {desc = 'Rename'})
+---- Auto indent on empty line.
+vim.keymap.set("n", "i", insertAndIndent, { expr = true, noremap = true })
 
 -- Clipboard system
 vim.keymap.set('v', '<leader>y',  '"+y')
