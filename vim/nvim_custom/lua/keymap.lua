a330fe15ff58ca8dea50c68b66c9d3d180172c03
Benoit Dumas <benoit@sogilis.com>
2024-11-26 09:54:04 +0100

    lazyvim: update

--- vim/nvim_custom/lua/keymap.lua
+++ vim/nvim_custom/lua/keymap.lua
@@ -1,25 +1,25 @@
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
 -- files to ignore with `file_ignore_patterns`
 local excludedFiles = {
-'yarn%.lock',
-'package%-lock%.json',
-'node_modules/.*',
-'%.git/.*',
+	"yarn%.lock",
+	"package%-lock%.json",
+	"node_modules/.*",
+	"%.git/.*",
 }
 local telescopeOpts = {
-    hidden = true,
-    follow = true,
-    file_ignore_patterns = excludedFiles,
-    path_display = { "truncate" }
+	hidden = true,
+	follow = true,
+	file_ignore_patterns = excludedFiles,
+	path_display = { "truncate" },
 }
 
-local findFile = function ()
-    builtin.find_files(telescopeOpts)
+local findFile = function()
+	builtin.find_files(telescopeOpts)
 end
 
-local searchFile = function ()
-    builtin.live_grep(telescopeOpts)
+local searchFile = function()
+	builtin.live_grep(telescopeOpts)
 end
 
 local insertAndIndent = function()
@@ -27,21 +27,21 @@ local insertAndIndent = function()
 end
 
 -- File navigation
-vim.keymap.set('n', '<C-p>', findFile, {desc = 'Find in Files'})
-vim.keymap.set('n', '<C-S-F>', searchFile, {desc = 'Files'})
-vim.keymap.set('n', '<leader>fr', builtin.buffers, {desc = 'Find buffer'})
+vim.keymap.set("n", "<C-p>", findFile, { desc = "Find in Files" })
+vim.keymap.set("n", "<C-S-F>", searchFile, { desc = "Files" })
+vim.keymap.set("n", "<leader>fr", builtin.buffers, { desc = "Find buffer" })
 
 -- Editor action
-vim.keymap.set('n', '<leader>r', ':IncRename ', {desc = 'Rename'})
+vim.keymap.set("n", "<leader>r", ":IncRename ", { desc = "Rename" })
 ---- Auto indent on empty line.
 vim.keymap.set("n", "i", insertAndIndent, { expr = true, noremap = true })
 
 -- Clipboard system
-vim.keymap.set('v', '<leader>y',  '"+y')
-vim.keymap.set('n', '<leader>Y',  '"+yg_')
-vim.keymap.set('n', '<leader>y',  '"+y')
-vim.keymap.set('n', '<leader>yy',  '"+yy')
-vim.keymap.set('n', '<leader>p', '"+p')
-vim.keymap.set('n', '<leader>P', '"+P')
-vim.keymap.set('v', '<leader>p', '"+p')
-vim.keymap.set('v', '<leader>P', '"+P')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+yg_')
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>yy", '"+yy')
+vim.keymap.set("n", "<leader>p", '"+p')
+vim.keymap.set("n", "<leader>P", '"+P')
+vim.keymap.set("v", "<leader>p", '"+p')
+vim.keymap.set("v", "<leader>P", '"+P')
