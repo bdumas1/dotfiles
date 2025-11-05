8cfbd68c2f90cc05cbf8847e24766dce6f0728dc
Benoit Dumas <benoit@sogilis.com>
2025-11-05 09:24:14 +0100

    hyprland: add remap workspaces script on hypr reload

---
+++ hypr/custom/remap_workspaces.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sleep 0.5
+hyprctl dispatch moveworkspacetomonitor 1 HDMI-A-1
+hyprctl dispatch moveworkspacetomonitor 2 HDMI-A-1
+hyprctl dispatch moveworkspacetomonitor 3 HDMI-A-1
+hyprctl dispatch moveworkspacetomonitor 4 HDMI-A-1
