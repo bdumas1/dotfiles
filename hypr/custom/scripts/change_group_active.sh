169b99eaf4c241f06cdf5814fc88508ad3358026
Benoit Dumas <benoit@sogilis.com>
2025-11-17 11:14:16 +0100

    hypr: add change_group_active script

---
+++ hypr/custom/scripts/change_group_active.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DIR="$1"
+
+if [[ "$DIR" != "left" && "$DIR" != "right" ]]; then
+    echo "Usage: $0 [left|right]"
+    exit 1
+fi
+
+active=$(hyprctl -j activewindow)
+group=$(echo "$active" | jq '.grouped')
+activeAddress=$(echo "$active" | jq '.address')
+firstGroupedAddress=$(echo "$active" | jq '.grouped[0]')
+lastGroupedAddress=$(echo "$active" | jq '.grouped[-1]')
+
+if [[ "$group" = "[]" ]]; then
+    exit 0
+fi
+
+if [[ "$DIR" == "left" ]]; then
+  if [[ "$firstGroupedAddress" == "$activeAddress" ]]; then
+    exit 0
+  fi
+
+  hyprctl dispatch changegroupactive b
+elif [[ "$DIR" == "right" ]]; then
+  if [[ "$lastGroupedAddress" == "$activeAddress" ]]; then
+    exit 0
+  fi
+
+  hyprctl dispatch changegroupactive f
+fi
